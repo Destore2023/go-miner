eb7409b4ad1c9dc020891fc4dba38a1d7fe58321
williams824 <produqzai824@gmail.com>
2021-02-16 10:58:06 +0800

    windows gen script

---
+++ rpc/proto/gen.bat
@@ -0,0 +1,21 @@
+REM API Source Code Generator for SKT Client API Developers
+REM Run this script ONLY on Windows
+REM See README.md for details
+set ver=v1.16.0
+protoc -I=%~dp0 ^
+    -I %GOPATH%/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@%ver%/third_party/googleapis ^
+    -I %GOPATH%/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@%ver% ^
+    --go_out=plugins=grpc:. ^
+    %~dp0\api.proto
+
+protoc -I=%~dp0 ^
+    -I %GOPATH%/pkg/mod/github.com\grpc-ecosystem\grpc-gateway@%ver%\third_party\googleapis ^
+    -I %GOPATH%/pkg/mod/github.com\grpc-ecosystem\grpc-gateway@%ver% ^
+    --grpc-gateway_out=logtostderr=true:. ^
+    %~dp0\api.proto
+
+protoc -I=%~dp0 ^
+    -I %GOPATH%/pkg/mod/github.com\grpc-ecosystem\grpc-gateway@%ver%\third_party\googleapis ^
+    -I %GOPATH%/pkg/mod/github.com\grpc-ecosystem\grpc-gateway@%ver% ^
+    --swagger_out=logtostderr=true:. ^
+    %~dp0\api.proto
